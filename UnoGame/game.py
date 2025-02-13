73fdec53c6ca65c70a6bf468f3d33c6544abf13e
komodoSam <shastri.jr@gmail.com>
2025-02-12 20:36:48 -0600

    Uno Game First Iteration

---
+++ UnoGame/game.py
@@ -0,0 +1,200 @@
+
+from player import Player
+from helpers import clear
+from card import Card
+
+class Game:
+    def __init__(self, player1: Player, player2: Player, player3: Player, player4: Player):
+        self.player1 = player1
+        self.player2 = player2
+        self.player3 = player3
+        self.player4 = player4
+        self.previous_card = Card()
+        self.turn = "clockwise" # can be clockwise (1, 2, 3, 4) or counterclockwise (4, 3, 2, 1)
+        self.previous_player = 4
+        self.player_override = 0 # override to this player
+        self.actions = None # do this first BEFORE prompting next user to pick card
+
+    def play(self):
+
+        while not self.check_winning():
+            next_player = self.get_next_player()
+            if next_player == 1:
+                self.player1_play()
+            elif next_player == 2:
+                self.player2_play()
+            elif next_player == 3:
+                self.player3_play()
+            elif next_player == 4:
+                self.player4_play()
+
+
+    def check_winning(self):
+        if len(self.player1.cards) == 0:
+            print("Player 1 wins!")
+            return True
+        elif len(self.player2.cards) == 0:
+            print("Player 2 wins!")
+            return True
+        elif len(self.player3.cards) == 0:
+            print("Player 3 wins!")
+            return True
+        elif len(self.player4.cards) == 0:
+            print("Player 4 wins!")
+            return True
+        else:
+            return False
+
+    def reverse(self):
+        if "clockwise" in self.turn:
+            self.turn = "counterclockwise"
+        else:
+            self.turn = "clockwise"
+
+    def skip(self):
+        # clockwise: 1 -> 3, 2 -> 4, 3 -> 1, 4 -> 2
+        # counterclockwise: 1 -> 3, 2 -> 4, 3 -> 1, 4 -> 2
+
+        if self.previous_player == 1:
+            self.player_override = 3
+        elif self.previous_player == 2:
+            self.player_override = 4
+        elif self.previous_player == 3:
+            self.player_override = 1
+        elif self.previous_player == 4:
+            self.player_override = 2
+
+    def get_next_player(self):
+        if self.player_override != 0:
+            player = self.player_override
+            self.player_override = 0
+            return player
+
+        if self.turn == "clockwise":
+            if self.previous_player == 4:
+                return 1
+            else:
+                return self.previous_player + 1
+        else:
+            if self.previous_player == 1:
+                return 4
+            else:
+                return self.previous_player - 1
+
+    def player1_play(self):
+        print("Player 1, please make your choice. ")
+        print("Current direction:", self.turn)
+
+        if self.actions == "+2":
+            self.player1.draw_card()
+            self.player1.draw_card()
+            print("You received 2 additional cards")
+            self.action = None
+        elif self.actions == "+4":
+            for i in range(4):
+                self.player1.draw_card()
+            print("You received 4 additional cards")
+            self.action = None
+        
+        self.player1.print_cards()
+        self.previous_card = self.player1.prompt_card(self.previous_card, self)
+        clear()
+        # if self.previous_player == 4:
+        #     self.previous_player = 1
+        self.previous_player = 1
+        if self.previous_card.special_ability == "reverse":
+            self.reverse()
+        elif self.previous_card.special_ability == "skip":
+            self.skip()
+        elif self.previous_card.special_ability == "+2":
+            self.actions = "+2"
+        elif self.previous_card.special_ability == "wild+4":
+            self.actions = "+4"
+
+    def player2_play(self):
+        print("Player 2, please make your choice. ")
+        print("Current direction: ", self.turn)
+
+        if self.actions == "+2":
+            self.player2.draw_card()
+            self.player2.draw_card()
+            print("You received 2 additional cards")
+            self.action = None
+        elif self.actions == "+4":
+            for i in range(4):
+                self.player2.draw_card()
+            print("You received 4 additional cards")
+            self.action = None
+
+        self.player2.print_cards()
+        self.previous_card = self.player2.prompt_card(self.previous_card, self)
+        clear()
+        # if self.previous_player == 1:
+        self.previous_player = 2
+        if self.previous_card.special_ability == "reverse":
+            self.reverse()
+        elif self.previous_card.special_ability == "skip":
+            self.skip()
+        elif self.previous_card.special_ability == "+2":
+            self.actions = "+2"
+        elif self.previous_card.special_ability == "wild+4":
+            self.actions = "+4"
+        
+    def player3_play(self):
+        print("Player 3, please make your choice. ")
+        print("Current direction: ", self.turn)
+
+        if self.actions == "+2":
+            self.player3.draw_card()
+            self.player3.draw_card()
+            print("You received 2 additional cards")
+            self.action = None
+        elif self.actions == "+4":
+            for i in range(4):
+                self.player3.draw_card()
+            print("You received 4 additional cards")
+            self.action = None
+
+        self.player3.print_cards()
+        self.previous_card = self.player3.prompt_card(self.previous_card, self)
+        clear()
+        # if self.previous_player == 2:
+        self.previous_player = 3
+        if self.previous_card.special_ability == "reverse":
+            self.reverse()
+        elif self.previous_card.special_ability == "skip":
+            self.skip()
+        elif self.previous_card.special_ability == "+2":
+            self.actions = "+2"
+        elif self.previous_card.special_ability == "wild+4":
+            self.actions = "+4"
+
+
+    def player4_play(self):
+        print("Player 4, please make your choice. ")
+        print("Current direction: ", self.turn)
+
+        if self.actions == "+2":
+            self.player4.draw_card()
+            self.player4.draw_card()
+            print("You received 2 additional cards")
+            self.action = None
+        elif self.actions == "+4":
+            for i in range(4):
+                self.player4.draw_card()
+            print("You received 4 additional cards")
+            self.action = None
+
+        self.player4.print_cards()
+        self.previous_card = self.player4.prompt_card(self.previous_card, self)
+        clear()
+        # if self.previous_player == 3:
+        self.previous_player = 4
+        if self.previous_card.special_ability == "reverse":
+            self.reverse()
+        elif self.previous_card.special_ability == "skip":
+            self.skip()
+        elif self.previous_card.special_ability == "+2":
+            self.actions = "+2"
+        elif self.previous_card.special_ability == "wild+4":
+            self.actions = "+4"
---
+++ UnoGame/helpers.py
@@ -0,0 +1,35 @@
+
+from card import Card
+from subprocess import call
+import os
+import subprocess
+
+SYSTEM_COLOR = "black" # this color is used for non-existent card
+
+def create_all_cards():
+    deck = []
+    for color in ["red", "green", "blue", "yellow"]:
+        for number in range(1, 10):
+            deck.append(Card(color, number, None))
+        for special_ability in ["reverse", "skip", "+2"]:
+            deck.append(Card(color, special_ability=special_ability))
+        
+    for color_spec in ["wild", "wild+4"]:
+        for i in range(4):
+            deck.append(Card(color="white", special_ability=color_spec))
+    
+    # deck.append(Card(special_ability="+4"))
+    # deck.append(Card(special_ability="+4"))
+    # deck.append(Card(special_ability="everything"))
+    # deck.append(Card(special_ability="everything"))
+
+    return deck
+
+def clear():
+    _ = call('clear' if os.name =='posix' else 'cls')
+    
+def clear():
+    if os.name == 'posix':
+        _ = subprocess.call('clear', shell=True)
+    else:  # os.name == 'nt' for Windows
+        _ = subprocess.call('cls', shell=True)
